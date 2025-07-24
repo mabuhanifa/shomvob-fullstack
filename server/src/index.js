8389939cd8c7f07ddd7c2dce93f384a7fb6f0f84
mabuhanifa <moh.abuhanifa@gmail.com>
2025-07-24 13:05:18 +0600

    configure CORS to allow specific origins

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://shomvob-fullstack.vercel.app/"],
+  })
+);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
